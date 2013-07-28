88282149f2244948daf31c174507d1008cb176dd
Nils Haldenwang <n.haldenwang@googlemail.com>
2013-07-28 17:06:22 +0200

    Railsapplikation angelegt.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mmog::Application.config.secret_key_base = '5d329ab38cae78cd503854a23507ae8211f6f124f2c8c36a4b981ae16b925d2e1d0d24e01559c132f691b6dad37a0ab2a47061a9eb0111d591b08e59475aa96e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mmog::Application.config.session_store :cookie_store, key: '_mmog_session'
