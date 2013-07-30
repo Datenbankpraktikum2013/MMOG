a609ac3b0773d20b46d268be49022872c2bce09f
Changeyourname <mloepmei@cip19.informatik.uni-osnabrueck.de>
2013-07-30 11:47:02 +0200

    put job createrors in planet

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,17 @@
+require "resque"
+require "models/workingqueues/buildBuildingqueue"
+require "models/workingqueues/rostoffqueue"
+
 class Planet < ActiveRecord::Base
+	
+	#grober entwurf
+	define createBuildingjob(buildingtyp_id)
+		Resque.enqueue(BuildBuildingjob, id_array(planet_id,buildingtyp_id))
+
+	end
+
+	define createRohstoffJob(buildingtyp_id)
+		Resque.enqueue(RohstoffProduktionsjob, planet_id)
+
+	end
 end
