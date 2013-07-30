ef8125f2e7310500b8cc0946cb4c95af197d2ead
mgegner <mgegner@cip16.informatik.uni-osnabrueck.de>
2013-07-30 15:15:37 +0200

    +Sunsystem fix +Planet.getDistance

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,6 +1,6 @@
-#require "resque"
-#require "app/controllers/workingqueues/buildBuildingqueue"
-#require "app/controllers/workingqueues/rohstoffqueue"
+require "resque"
+require "models/workingqueues/buildBuildingqueue"
+require "models/workingqueues/rostoffqueue"
 
 
 class Planet < ActiveRecord::Base
@@ -16,10 +16,41 @@ class Planet < ActiveRecord::Base
 	def createBuildingjob(buildingtyp_id)
 		Resque.enqueue(BuildBuildingjob, id_array(planet_id,buildingtyp_id))
 
-	end
+  belongs_to :sunsystem
+  has_many :buildings
 
-	def createRohstoffJob(buildingtyp_id)
-		Resque.enqueue(RohstoffProduktionsjob, planet_id)
+  #grober entwurf
+  def createBuildingjob(buildingtyp_id)
+    Resque.enqueue(BuildBuildingjob, id_array(planet_id,buildingtyp_id))
 
-	end
+  end
+
+  def createRohstoffJob(buildingtyp_id)
+    Resque.enqueue(RohstoffProduktionsjob, planet_id)
+
+  end
+
+  def getDistance(other)
+    if other.is_a?Planet then
+
+      dist1 = self.Sunsystem.getDistance(other.Sunsystem)
+      if dist1 < 0 then
+        return -1
+      end
+
+      if(dist1 == 0) then
+        if self.id < other.id then
+          dist2 = other.id - self.id
+        else
+          dist2 = self.id - other.id
+        end
+      else
+        dist2 = ((self.id + other.id)^3)/((self.id - other.id)^2 + 1)
+      end
+      dist1 + dist2
+
+    end
+    -1
+  end
+
 end
