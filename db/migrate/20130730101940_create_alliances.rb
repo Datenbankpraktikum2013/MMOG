478d6a8785db5f232b5721fb2808f59658ffd0c4
kobermann <kobermann@cip22.informatik.uni-osnabrueck.de>
2013-07-30 15:51:11 +0200

    test

--- db/migrate/20130730101940_create_alliances.rb
+++ db/migrate/20130730101940_create_alliances.rb
@@ -2,8 +2,6 @@ class CreateAlliances < ActiveRecord::Migration
   def change
     create_table :alliances do |t|
       t.string :name
-      t.integer :default_rank, :default => null
-
       t.timestamps
     end
   end
