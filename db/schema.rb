ba3253242a4891afef6230624d903d4b09303d90
kobermann <kobermann@cip22.informatik.uni-osnabrueck.de>
2013-07-31 09:47:05 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -59,26 +59,6 @@ ActiveRecord::Schema.define(version: 20130730140811) do
     t.datetime "updated_at"
   end
 
-  create_table "player_settings", force: true do |t|
-    t.float    "increased_income",            default: 1.0
-    t.float    "increased_ironproduction",    default: 1.0
-    t.float    "increased_energy_efficiency", default: 1.0
-    t.float    "increased_movement",          default: 1.0
-    t.float    "big_house",                   default: 1.0
-    t.float    "increased_research",          default: 1.0
-    t.float    "increased_power",             default: 1.0
-    t.float    "increased_defense",           default: 1.0
-    t.float    "increased_capacity",          default: 1.0
-    t.boolean  "hyperspace_technology",       default: false
-    t.boolean  "large_cargo_ship",            default: false
-    t.boolean  "large_defenseplattform",      default: false
-    t.boolean  "destroyer",                   default: false
-    t.boolean  "cruiser",                     default: false
-    t.boolean  "deathstar",                   default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "ranks", force: true do |t|
     t.string   "name"
     t.boolean  "can_kick"
