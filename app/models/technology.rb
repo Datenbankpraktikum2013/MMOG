73ffbb8327a07a2fe4e59b94a1c4381367f75609
JonasRothe <online@rothejs.de>
2013-07-31 13:27:26 +0200

    adds get and has methods for usersettings

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -3,7 +3,18 @@ class Technology < ActiveRecord::Base
   has_and_belongs_to_many :ships
   has_many :user_technologies
 
-  def upgrade_technology(user)
-                    #TODO
+  def upgrade_technology(user, tech)
+
+
+
+    result = user_technologies.where(:user_id => user, :technology_id => tech).first
+
+    if result!=nil then
+      rank = result.rank
+      result.update_attribute(:rank, rank+1)
+    else
+      create()
+      user_technologies.create(:user_id => user, :technology_id => tech, :rank => 1)
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,70 @@ class User < ActiveRecord::Base
   		errors.add :username, "is already taken."
   	end
   end
+
+  #get user-settings affected by Research
+  def get_movement
+    self.user_setting.increased_movement
+  end
+
+  def get_income
+    self.user_setting.increased_income
+  end
+
+  def get_ironproduction
+    self.user_setting.increased_ironproduction
+  end
+
+  def get_energy_efficiency
+    self.user_setting.increased_energy_efficiency
+  end
+
+  def get_research
+    self.user_setting.increased_research
+  end
+
+  def get_power
+    self.user_setting.increased_power
+  end
+
+  def get_defense
+    self.user_setting.increased_defense
+  end
+
+  def get_capacity
+    self.user_setting.increased_capacity
+  end
+
+  def get_spypower
+    self.user_setting.increased_spypower
+  end
+
+  def get_big_house
+    self.user_setting.big_house
+  end
+
+  def has_hyperspace_technology?
+    self.user_setting.hyperspace_technology
+  end
+
+  def has_large_cargo_ship?
+    self.user_setting.large_cargo_ship
+  end
+
+  def has_large_defenseplattform?
+    self.user_setting.large_defenseplattform
+  end
+
+  def has_destroyer?
+    self.user_setting.destroyer
+  end
+
+  def has_cruiser?
+    self.user_setting.cruiser
+  end
+
+  def has_deathstar?
+    self.user_setting.deathstar
+  end
+
 end
