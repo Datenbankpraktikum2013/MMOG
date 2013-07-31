748a1d2936c18f7c785c75aa551e612c5e87d865
kobermann <kobermann@cip22.informatik.uni-osnabrueck.de>
2013-07-31 11:29:48 +0200

    ...

--- db/migrate/20130730101940_create_alliances.rb
+++ db/migrate/20130730101940_create_alliances.rb
@@ -1,7 +1,8 @@
 class CreateAlliances < ActiveRecord::Migration
   def change
     create_table :alliances do |t|
-      t.string :name
+      t.string  :name
+      t.text 	:description
       t.timestamps
     end
   end
