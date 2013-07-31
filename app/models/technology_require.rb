bbbf03e3a7a7b05b932e03f5ed64c6118f90a43b
JonasRothe <online@rothejs.de>
2013-07-31 13:21:47 +0200

    method technology_require added

--- app/models/technology_require.rb
+++ app/models/technology_require.rb
@@ -3,4 +3,14 @@ class TechnologyRequire < ActiveRecord::Base
   belongs_to :tech, :class_name => "Technology"
   belongs_to :pre_tech, :class_name => "Technology"
 
+
+  def technology_require? (techno, user)
+    #TODO   chech the requirements for a specific technology -- return true if fulfilled
+
+    #
+      
+
+  end
+
+
 end
