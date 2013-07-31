45374c66cfc1cf1aaa5c15f990f5a3391c9b03fe
hkroemke <hkroemke@cip20.informatik.uni-osnabrueck.de>
2013-07-31 15:24:51 +0200

    production alpha

--- app/jobs/produce_resources.rb
+++ app/jobs/produce_resources.rb
@@ -9,7 +9,7 @@ class ProduceResources
 		puts "der Planet hat vor erhöhen #{@planet.ore}"
 
 		#noch nicht implementiert stand 11:07
-		@planet.updateResources
+		@planet.update_resources
 		@planet.save
 		puts "der Planet hat nach erhöhen #{@planet.ore}"
 	end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -4,50 +4,90 @@ class Planet < ActiveRecord::Base
   belongs_to :sunsystem
   has_many :buildings
   has_many :fleets
-  def getProductionFactorOf(type)
+  def get_production_factor_of(type)
     # TODO Calculate the factor of productionspeed
     return 1
   end
 
-  def getBuildingFactorOf(type)
+  def get_building_factor_of(type)
     # TODO Calculate the factor of buildingspeed
     return 1
   end
 
   #Method which increases and updates all the resources a player has every ...Minute
-  def updateResources
-=begin
-    oremine = self.Buildings.find_by name: 'Oremine'
-    ore_production = oremine.production
+  def update_resources
+    if energy > 0 
+      oremine = self.Buildings.find_by name: 'Oremine'
+      ore_production = oremine.production
+      energyneeded = oremine.eneryusage; 
+      f = getProductionFactorOf(:ore)
 
-    f = getProductionFactorOf(:ore)
-
-    #Ore production
-    if ore_production.integer? then
-      ore_production = f * ore_production
-      if (self.ore + ore_production) < self.maxore then
-        self.ore += ore_production
-      else
-        self.ore = self.maxore
+      #Ore production
+      if ore_production.integer? then
+        ore_production = f * ore_production
+        if (self.ore + ore_production) < self.maxore then
+          self.ore += ore_production
+        else
+          self.ore = self.maxore
+        end
+        #Resque.enqueue_in(1.minute, ProduceResources, self.id)
       end
-      #Resque.enqueue_in(1.minute, ProduceResources, self.id)
+
+
+      city = Buildings.find_by name: 'City'
+      city_population = city.production
+      energyneeded += city.eneryusage;
+
+      if city_population.integer? then
+        #f???
+        if (self.population + city_population) < self.maxpopulation then
+          self.population += city_population
+        else
+          self.population = self.maxpopulation
+        end
+      end
+      #Berechnung der Steuern der Einwohner. Start population sollte > 1000 sein
+      taxes = (self.population / 100)
+      headquarter = self.Buildings.find_by name: 'Headquarter'
+      energyneeded += headquarter.eneryusage;
+      headquarter_lev = headquarter.level
+
+      earns = taxes * headquarter_lev
+
+      owner = User.find_by id: self.user_id
+      owner.money += earns
     end
-
-
-    city = Buildings.find_by name: 'City'
-    taxes = city.production
-
-
-    self.createProductionJob
+=begin
+  STROMKOSTEN DER RESTLICEHN GEBÄUDE BERECHNEN
 =end
+
+      #Berechnung der benoetigten Energie 
+      powerplant = Buildings.find_by name: "Powerplant"
+      power = powerplant.production
+      #Reduziere um benötigte Gebauudenergy
+      if energyneeded.integer?
+        power -= energyneeded
+      end  
+      energy += power
+      if energy.integer? then
+        #f???
+        if (self.energy + power) < self.maxenergy then
+          self.energy += power
+        else
+          self.energy = self.maxenergy
+        end
+      end
+    else  
+    self.create_production_job
+
   end
 
   #grober entwurf
-  def createBuildingJob(buildingtyp_id)
+  def create_building_job(buildingtyp_id)
     Resque.enqueue(BuildBuildingjob, id_array(planet_id,buildingtyp_id))
   end
 
-  def createProductionJob()
+  def create_production_job()
     puts "Planeten ID #{self.id}"
     #Resque.enqueue(ProduceResources, self.id)
     Resque.enqueue_in(1.minute, ProduceResources, self.id)
