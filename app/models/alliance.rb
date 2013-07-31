b6e5ea4ff8f88e1deb6c55149b52a3eca92b0095
kobermann <kobermann@cip22.informatik.uni-osnabrueck.de>
2013-07-31 16:34:42 +0200

    merge

--- app/models/alliance.rb
+++ app/models/alliance.rb
@@ -9,7 +9,7 @@ class Alliance < ActiveRecord::Base
 #checks if alliance name is already taken
 	def is_alliance_name_taken
 		if Alliance.exists?(:name.downcase => name.downcase)
-		errors.add :name, "is already taken"
+			errors.add :name, "is already taken"
 		end
 	end		
 end
