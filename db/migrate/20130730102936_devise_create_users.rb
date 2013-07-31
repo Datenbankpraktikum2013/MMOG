a13b332edecf6e5986e4d52a40c1f90927426d10
rhelmke <rhelmke@cip21.informatik.uni-osnabrueck.de>
2013-07-31 11:29:57 +0200

    merge

--- db/migrate/20130730102936_devise_create_users.rb
+++ db/migrate/20130730102936_devise_create_users.rb
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Token authenticatable
       # t.string :authentication_token
 
-      t.string :nickname,           :null => false
+      t.string :username,           :null => false
       t.integer :money,             :default => 100
       t.integer :score,             :default => 0
       t.integer :alliance_id
@@ -42,9 +42,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :users, :email,                :unique => true
+    add_index :users, :email, :username      :unique => true
     add_index :users, :reset_password_token, :unique => true
-    add_index :users, :username,             :unique => true
     # add_index :users, :confirmation_token,   :unique => true
     # add_index :users, :unlock_token,         :unique => true
     # add_index :users, :authentication_token, :unique => true
--- db/schema.rb
+++ db/schema.rb
@@ -177,7 +177,7 @@ ActiveRecord::Schema.define(version: 20130731082435) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "nickname",                             null: false
+    t.string   "username",                             null: false
     t.integer  "money",                  default: 100
     t.integer  "score",                  default: 0
     t.integer  "alliance_id"
@@ -188,5 +188,6 @@ ActiveRecord::Schema.define(version: 20130731082435) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
