580a9e58f3ed5de040c2d845d8c88e5bde9d8e15
A. Loehr <aloehr@uos.de>
2013-07-31 16:10:17 +0200

    added virtual Attr amount to ship

--- app/models/fleet.rb
+++ app/models/fleet.rb
@@ -1,6 +1,6 @@
 class Fleet < ActiveRecord::Base
 	has_many :shipfleets
-	has_many :ships, :through => :shipfleets
+	has_many :ships, :through => :shipfleets, :select => "ships.*, shipfleets.amount"
 	belongs_to :start_planet, class_name: "Planet", foreign_key: "start_planet"
 	belongs_to :target_planet, class_name: "Planet", foreign_key: "target_planet"
 	belongs_to :user
