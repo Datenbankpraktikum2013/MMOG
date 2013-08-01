3aff8acd0f31d103c8a50987d1e9f91d41c0a8dc
kobermann <kobermann@cip22.informatik.uni-osnabrueck.de>
2013-08-01 15:01:49 +0200

    merge

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -4,16 +4,12 @@ class Rank < ActiveRecord::Base
 
 	belongs_to :alliance
 
-	validates :is_rank_name_taken, on: :create, on: :update
+	#validate :is_rank_name_taken, on: :create, on: :update
 
 #check if rank name is taken
 	def is_rank_name_taken
-		@ranks=Rank.find_by_alliance_id(current_user.alliance_id)
-		@ranks.each do |rank|
-			if rank.name.downcase==:name.downcase
-				errors.add :name "already exists in your alliance"
-			end
-		end		
+		
+			errors.add :name, " already exists in your alliance"
+			
 	end
-
 end
