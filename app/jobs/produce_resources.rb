ab5241d5f9ed2038b1ab999ab326414931623309
rubyiskillingme <mloepmei@cip19.informatik.uni-osnabrueck.de>
2013-08-02 15:43:27 +0200

    Ruby fucked my live #resqueworksagain

--- app/jobs/produce_resources.rb
+++ app/jobs/produce_resources.rb
@@ -3,11 +3,9 @@
 class ProduceResources
 	@queue = "workqueue"
 	def self.perform(id)
-		#puts "Rohstoff produktion auf #{id}"
+		#raise "Hallo"
 		@planet = Planet.find_by_id(id)
-		#puts "der Planet hat vor erhöhen #{@planet.ore}"
 		@planet.update_resources
-		#puts "der Planet hat nach erhöhen #{@planet.ore}"
 		@planet.save
 	end
 
