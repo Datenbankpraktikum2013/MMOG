3a7382327817900db126e82412c3322e5fb8dddb
rubyiskillingme <mloepmei@cip19.informatik.uni-osnabrueck.de>
2013-08-06 14:56:19 +0200

    building Buildings works now

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -27,76 +27,77 @@ class Planet < ActiveRecord::Base
     7 forschungfactor
 =end
     @spec = [1, 1, 1, 1, 1, 1, 1, 1]
-    
-    self.size = Random.rand(MAX_SIZE-MIN_SIZE) + MIN_SIZE if self.size.nil?
-    self.ore = 20 if self.ore.nil?
-    self.maxore = 100 if self.maxore.nil?
-    self.maxcrystal = 1 if self.maxcrystal.nil?
-    self.maxenergy = 200 if self.maxenergy.nil?
-    self.crystal = 0 if self.crystal.nil?
-    self.energy = 50 if self.energy.nil?
-    self.population = self.size/10 if self.population.nil?
-    self.maxpopulation = self.size/2 if self.maxpopulation.nil?
-
-    #Creates random Planet name
     if self.name.nil?
-      self.name = PlanetsHelper.namegen
-    end
+      self.size = Random.rand(MAX_SIZE-MIN_SIZE) + MIN_SIZE if self.size.nil?
+      self.ore = 20 if self.ore.nil?
+      self.maxore = 100 if self.maxore.nil?
+      self.maxcrystal = 1 if self.maxcrystal.nil?
+      self.maxenergy = 200 if self.maxenergy.nil?
+      self.crystal = 0 if self.crystal.nil?
+      self.energy = 50 if self.energy.nil?
+      self.population = self.size/10 if self.population.nil?
+      self.maxpopulation = self.size/2 if self.maxpopulation.nil?
 
-    #Creates specialties for Planet
-    unless self.special.nil? || self.special == 0
-      self.special = Random.rand(7) + 1
-      #Oreplanet
-      if self.special == 1
-         self.ore = 50
-         @spec[0] = 1.3
-      #Loveplanet   
-      elsif self.special == 2
-         @spec[1] = 1.3
-      #Creditplanet   
-      elsif self.special == 3
-         @spec[3] = 1.3
-      #Crystalplanet   
-      elsif self.special == 4
-         self.size = MIN_SIZE + Random.rand(3000)
-         self.population = self.size/10
-         self.maxpopulation = self.size/2
-         self.maxore = 75
-         self.maxenergy = 175
-         self.maxcrystal = 5
-         @spec = [0.5, 0.5, 0.5, 0.5, 1, 1, 1,1]
-      #Buildplanet
-      elsif self.special == 5
-         self.ore = 50
-         self.energy = 100
-         @spec[6] = 0.7
-      #Lagerplanet   
-      elsif self.special == 6
-         self.maxore = 200
-         self.maxenergy = 400
-         self.maxcrystal = 5
-         @spec[5] = 1.3
-      #Scienceplanet
-      elsif self.special == 7
-         @spec[7] = 1.3
-      #Energieplanet   
-      else self.special == 8
-        self.energy = 100
-         @spec[2] = 1.3
-      end   
-    else
-       
-      self.size = (MAX_SIZE/2)
-      self.ore = 20
-      self.special = 0
-      self.maxore = 100
-      self.maxcrystal = 1
-      self.maxenergy = 200
-      self.crystal = 0
-      self.energy = 50
-      self.population = 1000
-      self.maxpopulation = 5000
+      #Creates random Planet name
       
+        self.name = PlanetsHelper.namegen
+      
+
+      #Creates specialties for Planet
+      unless self.special.nil? || self.special == 0
+        self.special = Random.rand(7) + 1
+        #Oreplanet
+        if self.special == 1
+           self.ore = 50
+           @spec[0] = 1.3
+        #Loveplanet   
+        elsif self.special == 2
+           @spec[1] = 1.3
+        #Creditplanet   
+        elsif self.special == 3
+           @spec[3] = 1.3
+        #Crystalplanet   
+        elsif self.special == 4
+           self.size = MIN_SIZE + Random.rand(3000)
+           self.population = self.size/10
+           self.maxpopulation = self.size/2
+           self.maxore = 75
+           self.maxenergy = 175
+           self.maxcrystal = 5
+           @spec = [0.5, 0.5, 0.5, 0.5, 1, 1, 1,1]
+        #Buildplanet
+        elsif self.special == 5
+           self.ore = 50
+           self.energy = 100
+           @spec[6] = 0.7
+        #Lagerplanet   
+        elsif self.special == 6
+           self.maxore = 200
+           self.maxenergy = 400
+           self.maxcrystal = 5
+           @spec[5] = 1.3
+        #Scienceplanet
+        elsif self.special == 7
+           @spec[7] = 1.3
+        #Energieplanet   
+        else self.special == 8
+          self.energy = 100
+           @spec[2] = 1.3
+        end   
+      else
+         
+        self.size = (MAX_SIZE/2)
+        self.ore = 20
+        self.special = 0
+        self.maxore = 100
+        self.maxcrystal = 1
+        self.maxenergy = 200
+        self.crystal = 0
+        self.energy = 50
+        self.population = 1000
+        self.maxpopulation = 5000
+        
+      end
     end
   end
 
@@ -136,28 +137,33 @@ class Planet < ActiveRecord::Base
     if type == :Oremine
       #sci_factor = self.user.get_ironproduction
       c = sci_factor * prod * @spec[0]
+      puts "Ore+#{c}"
       return c
     end
 
     if type == :City
       c = @spec[2] * prod
+      puts "people +#{c}"
       return c
     end
 
     if type == :Headquarter
       #sci_factor = self.user.get_income
       c = sci_factor * @spec[3] * (self.population / 100)# * prod 
+      puts "money +#{c}"
       return c
     end
 
     if type == :Powerplant
       #sci_factor = self.user.get_energy_efficiency
       c = sci_factor * prod * @spec[1] 
+      puts "Energy +#{c}"
       return c
     end
 
     if type == :Crystalmine
       c = @spec[4] * prod
+      puts "Kristalle +#{c}"
       return c
     end
   end
@@ -181,6 +187,7 @@ class Planet < ActiveRecord::Base
         else
           self.ore = self.maxore
         end
+        puts "ERZ 1 jetzt: #{self.ore}"
       end
 
       #
@@ -206,7 +213,8 @@ class Planet < ActiveRecord::Base
           self.crystal = self.maxcrystal
         end
       end
-
+      self.save
+      puts "#ERZ JETZT: #{self.ore}"
       #
       # updates money 
       #
@@ -214,26 +222,32 @@ class Planet < ActiveRecord::Base
       owner = self.user
       owner.money += income
       owner.save
+      
+
+      # Energy costs 
+      @structures = self.buildings
+      ener_usage = 0
+      @structures.each do |str|
+        building_typ = Buildingtype.find_by_id(str.buildingtype_id)
+        ener_usage += building_typ.energyusage
+        self.energy -= ener_usage
+      end 
+
     end
 
-    #
-    # Energy costs and production
-    #
-    power_production = self.get_production(:Powerplant)
-    if (self.energy + power_production) < self.maxenergy
-      self.energy += power_production
-    else
-      self.energy = self.maxenergy
-    end
     
-    @structures = self.buildings
-    ener_usage = 0
-    @structures.each do |str|
-      building_typ = Buildingtype.find_by_id(str.buildingtype_id)
-      ener_usage += building_typ.energyusage
-    end
+    
 
-    self.energy -= ener_usage 
+      #
+      # Energy costs and production
+      #
+      power_production = self.get_production(:Powerplant)
+      if (self.energy + power_production) < self.maxenergy
+        self.energy += power_production
+      else
+        self.energy = self.maxenergy
+      end
+    
  
     # Repeat Job imediately
     self.create_production_job
@@ -253,10 +267,11 @@ class Planet < ActiveRecord::Base
       build_me = Buildingtype.where(name: type.to_s, level:1).first.id
       puts "1: " + build_me.to_s
     else  
-      upgrade_me = upgrade_me.id
+      upgrade_me = upgrade_me.buildingtype_id
+      my_future_me = Buildingtype.find_by_id(upgrade_me)
       puts "2: "+ upgrade_me.to_s
       #build_time = Buildingtype.where(name:type level:upgrade_me.level+1).build_time
-      #build_me = Buildingtype.where(name:type level:upgrade_me.level+1).id
+      build_me = Buildingtype.where(name:type ,level:(my_future_me.level)+1).first.id
     end
     id_array = []
     id_array << self.id
@@ -266,7 +281,7 @@ class Planet < ActiveRecord::Base
 
   end
 
-  def build(buildingtype_id)
+  def build_building(buildingtype_id)
     #destroy_me = self.buildings.where(name: Buildingtype.where(id: id).first.name).first.id
     #destroy_me.destroy unless destroy_me.nil?
     #reborn_me = Building.create(buildingtype_id: id, planet: seld.id)
@@ -277,6 +292,9 @@ class Planet < ActiveRecord::Base
     buildings.each do |b|
       if b.buildingtype.name == build_me.name && b.buildingtype.level + 1 == build_me.level
         b.buildingtype = build_me
+
+        b.save
+        puts b.buildingtype.level
         return true
       end
     end
