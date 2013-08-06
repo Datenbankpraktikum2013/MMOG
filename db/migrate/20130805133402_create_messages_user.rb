770871157c2190cd53ba94e77ec8aa7315f5742c
René Helmke <rhelmke@uni-osnabrueck.de>
2013-08-06 14:26:24 +0200

    Delete 20130805133402_create_messages_user.rb

--- db/migrate/20130805133402_create_messages_user.rb
+++
@@ -1,9 +0,0 @@
-class CreateMessagesUsers < ActiveRecord::Migration
-  def change
-    create_table :messages_user do |t|
-    	t.belongs_to :user
-     	t.belongs_to :message
-     	t.boolean	 :read, default: false
-    end
-  end
-end
