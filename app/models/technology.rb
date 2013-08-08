aba496c2d5d23f188074ee98beef1ec33435fd8e
JonasRothe <online@rothejs.de>
2013-08-08 15:31:47 +0200

    adds user notification for finished researches

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -70,6 +70,7 @@ class Technology < ActiveRecord::Base
 
     #Setze User-Forschung auf 0 -> Ende
     user.user_setting.update_attribute :researching, 0
+    user.system_notify( 'Forschung', title, ' Technologie: '+title+' Stufe '+new_rank.to_s+' erfolgreich erforscht.')
   end
 
   #Prüft nacheinander alle Vorraussetzungen der Technology für einen user
