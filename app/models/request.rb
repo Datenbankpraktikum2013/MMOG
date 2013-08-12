aee7a3eac2f20409f8030b7eaea9ee74435722d4
Rene Helmke <rhelmke@uni-osnabrueck.de>
2013-08-12 11:01:20 +0200

    Requestsystem alpha implemented

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,6 +4,7 @@ class Request < ActiveRecord::Base
   belongs_to :recipient, :class_name => 'User', :foreign_key => 'recipient_id'
   validates_presence_of :sender_id,:recipient_id
 
+  @@actions=Hash.new
   private
   	def calculate_requestvalue!()
   		self.requestvalue=Digest::SHA1.hexdigest([Time.now, rand].join)
@@ -12,13 +13,25 @@ class Request < ActiveRecord::Base
 
   public
   	def decide_notify()
+      answerbutton="Für was entscheidest du dich? <br><a class=\"label label-success\" data-method=\"post\" href=\"/requests/reaction/?answer=yes&for="+self.requestvalue+"\" rel=\"nofollow\" style=\"color:#FFFFFF;\">Annehmen</a><a class=\"label label-warning\" data-method=\"post\" href=\"/requests/reaction/?answer=no&for="+self.requestvalue+"\" rel=\"nofollow\" style=\"color:#FFFFFF;\">Ablehnen</a> "
   		if self.action=="alliance_invite"
-  			self.recipient.system_notify("Allianzeinladung","Die Allianz "+self.sender.alliance.name+" würde gerne zusammen mit dir spielen!"  				,"Hallo! "+self.sender.username+" hat dich eingeladen, seiner Allianz beizutreten. Für was entscheidest du dich?<br><a class=\"label label-success\" data-method=\"post\" href=\"/requests/reaction/?answer=yes&for="+self.requestvalue+"\" rel=\"nofollow\" style=\"color:#FFFFFF;\">Annehmen</a><a class=\"label label-warning\" data-method=\"post\" href=\"/requests/reaction/?answer=no&for="+self.requestvalue+"\" rel=\"nofollow\" style=\"color:#FFFFFF;\">Ablehnen</a> ")
+  			self.recipient.system_notify("Allianzeinladung","Die Allianz "+self.sender.alliance.name+" würde gerne zusammen mit dir spielen!"  				,answerbutton)
+      elsif self.action=="friendship_invite"
+        self.recipient.system_notify("Freundschaftsanfrage",self.sender.username,"Hallo! "+self.sender.username+" würde gerne mit dir befreundet sein."+answerbutton )
       end
   	end
 
   public
-    def accept()
-      
+    def launch_action!()
+      if self.action=="alliance_invite"
+        self.sender.alliance.add_user(recipient)
+        self.sender.system_notify("Allianzeinladung",self.recipient.username+" ist nun Teil deiner Allianz!","Tolle Wurst.")
+        self.destroy
+      elsif self.action=="friendship_invite"
+        self.sender.make_friendship!(self.recipient)
+        self.sender.system_notify("Freundschaftsanfrage",self.recipient.username+" hat deine Anfrage angenommen!","Tolle Wurst.")
+        self.destroy
+      end
     end
+    
 end
